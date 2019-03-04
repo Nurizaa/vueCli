6720af182a70d90c2ecdbff28e368b51bde51f4b
sherkazy <sheki92m@mail.ru>
2019-03-04 19:16:56 +0600

    API-ден кантип дата (JSON форматта) алганды жасадым

---
+++ src/views/Test.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="about">
+        <h1>{{info}}</h1>
+    </div>
+</template>
+
+<script>
+    import axios from 'axios'
+    export default {
+        el: '#test',
+        data() {
+            return {
+                info: null
+            };
+        },
+        mounted() {
+            axios
+                .get('https://api.coindesk.com/v1/bpi/currentprice.json')
+                .then(response => (this.info = response));
+        }
+    };
+</script>
